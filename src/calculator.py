560e81bc11f55469172db7be974cabb99988639b
vlad <admin@Admins-MacBook-Pro.local>
2023-07-31 11:54:39 +0300

    optimize by PEP8

--- src/calculator.py
+++ src/calculator.py
@@ -1,7 +1,7 @@
 # Simple Strategy design pattern
 
 class Calculator:
-    def calculate(self, operator, operand1, operand2):
+    def calculate(self, operand1, operand2):
         raise NotImplementedError("Subclasses must implement the 'calculate' method.")
 
 
@@ -9,15 +9,18 @@ class AddOperator(Calculator):
     def calculate(self, operand1, operand2):
         return operand1 + operand2
 
+
 class SubtractOperator(Calculator):
     def calculate(self, operand1, operand2):
         return operand1 - operand2
+
 
 class DivideOperator(Calculator):
     def calculate(self, operand1, operand2):
         if operand2 == 0:
             raise ZeroDivisionError("Division by zero is not allowed.")
         return operand1 / operand2
+
 
 class MultiplyOperator(Calculator):
     def calculate(self, operand1, operand2):
